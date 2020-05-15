90c066c2ba787e523cc3b60261e8c8aa04f3fa95
Saeed <52499523+saeed1986@users.noreply.github.com>
2020-05-15 17:00:23 +0200

    the Files what i need
    my first App per Vue.js

---
+++ vue.pro1/original (5)/who_pays_the_bill/js/main.js
@@ -0,0 +1,42 @@
+new Vue({
+  el: "#app",
+  data: {
+    state: true,
+    inputName: " ",
+    names: [],
+    showError: false,
+    result: "",
+  },
+  methods: {
+    addNameToList() {
+      if (this.validate(this.inputName)) {
+        this.names.push(this.inputName);
+        this.inputName = "";
+        this.showError = false;
+      } else {
+        this.showError = true;
+      }
+    },
+    removeName(index) {
+      this.names.splice(index, 1);
+    },
+    validate(value) {
+      if (value !== "") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    showResults() {
+      let rand = this.names[Math.floor(Math.random() * this.names.length)];
+      this.result = rand;
+      this.state = false;
+    },
+    resetApp() {
+      this.state = true;
+      this.inputName = "";
+      this.names = [];
+      this.result = "";
+    },
+  },
+});
